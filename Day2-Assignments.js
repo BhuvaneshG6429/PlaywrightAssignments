f65de7ab095e2a93ac63a40f21d7729762c80f95
BhuvaneshG6429 <85409725+BhuvaneshG6429@users.noreply.github.com>
2026-07-08 14:52:09 +0530

    Add files via upload

---
+++ Day2-Assignments.js
@@ -0,0 +1,19 @@
+let userName = "Bhuvanesh"
+console.log("user name is "+userName)
+console.log("Datatype of user name is "+typeof userName)
+
+let organization = "CTS"
+console.log("organization is "+organization)
+console.log("Datatype of organization is "+typeof organization)
+
+let contactNumber = 9999999999
+console.log("contact number is "+contactNumber)
+console.log("Datatype of contactNumber is "+typeof contactNumber)
+
+let knowsAutomation = true
+console.log("Status of knows automation is "+knowsAutomation)
+console.log("Datatype of knowsAutomation is "+typeof knowsAutomation)
+
+let usesPlaywright
+console.log("Status of uses playwright is "+usesPlaywright)
+console.log("Datatype of uses playwright is "+typeof usesPlaywright)
