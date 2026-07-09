49d5828006fcce418c98277d0a0ff027cbbe762d
BhuvaneshG6429 <85409725+BhuvaneshG6429@users.noreply.github.com>
2026-07-09 18:44:06 +0530

    Add files via upload
    My playwright assignments-Day3

---
+++ Week1Day2/javascriptbasics.js
@@ -0,0 +1,19 @@
+//console.log("Wohoo")
+
+const a = "Wohoo"
+console.log(a)
+
+const number = 1234
+
+const b = 21.124
+const c = false
+let d
+const e = null
+const f = 13909846940692767678467927627057486790505463463473738362238893636n
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
---
+++ Week1Day3/functions.js
@@ -0,0 +1,26 @@
+
+function add( a, b){
+    console.log(a+b);
+    
+}
+
+add(2,3)
+add(5,7)
+
+let a = 2
+let c = 3
+if(a==1 && c==3){
+    let b=3
+    console.log(a);
+    
+}
+else if(a==2){
+    console.log(a);
+    
+}
+
+else{
+    console.log("a is not defined")
+}
+
+console.log(c)
